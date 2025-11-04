711d30240dd21d7a07b113f35f461b2fc488f5a8
regina <regina-will-programmieren@web.de>
2025-11-04 19:52:22 +0100

    manifest.json added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function calc(){
-    alert ('Hallo');
+
+function calc() {
+    //greift auf die id euroField zu
+  let euro = euroField.value;
+  // gibt der Variablen result das Ergebnis
+  let result = euro * 0.981;
+  // gibt das Ergebnis bei id=usd aus
+  usd.value = result.toFixed(2).replace('.',',');
 }
+
+//https://www.youtube.com/watch?v=kAEt6JxrkjI
